90ea072dbc690200e45257e21a8a27308a7a8e9c
Victor Ibañez Emborujo <28122002victor@gmail.com>
2022-01-25 14:17:01 +0100

    Correcccion ej 1,2 y 3 y realización del resto

--- SQL/historial.sql
+++ SQL/historial.sql
@@ -202,6 +202,14 @@ EMP_NO TRA
 3333   T5 
 4444   T3 
 */
+--mismo resultado:--
+/*
+SELECT emp_no, trab_cod
+FROM historial_laboral
+UNION 
+SELECT emp_no, trab_cod
+FROM empleados;
+*/
 
 /*Ejercicio_2:*/
 /*Lista los empleados que aparte de haber cambiado de puesto, también han cambiado 
@@ -211,12 +219,49 @@ del departamento en el que desarrolla y desarrollaba cada trabajo. Si un emplead
 ha desarrollado mas de una vez un mismo trabajo, esta información aparecerá. 
 Mostrar la salida ordenada por departamento.*/
 
-SELECT e.emp_no, e.trab_cod, d.dept_no, d.dnombre
+SELECT e.emp_no, e.trab_cod, e.dpto_cod, d.dnombre
 FROM empleados e, depart d
 WHERE e.dpto_cod = d.dept_no
 UNION ALL
-SELECT emp_no, trab_cod
-FROM historial_laboral;
+SELECT h.emp_no, h.trab_cod, h.dpto_cod, d.dnombre
+FROM historial_laboral h, departs d;
+WHERE h.dpto_cod = d.dept_no
+ORDER BY dpto_cod;
+/*
+EMP_NO TRA   DPTO_COD DNOMBRE       
+------ --- ---------- --------------
+4444   T3          10 CONTABILIDAD  
+1111   T1          10 INVESTIGACION 
+1111   T2          20 INVESTIGACION 
+1111   T1          10 INVESTIGACION 
+2222   T1          10 INVESTIGACION 
+2222   T5          10 INVESTIGACION 
+3333   T2          30 INVESTIGACION 
+3333   T5          20 INVESTIGACION 
+4444   T3          10 INVESTIGACION 
+1111   T1          10 VENTAS        
+1111   T2          20 VENTAS        
+
+EMP_NO TRA   DPTO_COD DNOMBRE       
+------ --- ---------- --------------
+1111   T1          10 VENTAS        
+2222   T1          10 VENTAS        
+2222   T5          10 VENTAS        
+3333   T2          30 VENTAS        
+3333   T5          20 VENTAS        
+4444   T3          10 VENTAS        
+1111   T1          10 PRODUCCION    
+1111   T2          20 PRODUCCION    
+1111   T1          10 PRODUCCION    
+2222   T1          10 PRODUCCION    
+2222   T5          10 PRODUCCION    
+
+EMP_NO TRA   DPTO_COD DNOMBRE       
+------ --- ---------- --------------
+3333   T2          30 PRODUCCION    
+3333   T5          20 PRODUCCION    
+4444   T3          10 PRODUCCION 
+*/
 
 /*Ejercicio_3:*/
 /*Visualizar los códigos de los empleados y de los trabajos de los empleados que 
@@ -224,6 +269,16 @@ actualmente tengan un trabajo que ocuparon ya anteriormente en la compañía.*/
 
 SELECT emp_no, trab_cod
 FROM empleados
+INTERSECT
+SELECT emp_no, trab_cod
+FROM historial_laboral;
+/*
+EMP_NO TRA
+------ ---
+1111   T2 
+2222   T1 
+4444   T3 
+*/
 
 
 /*Ejercicio_4:*/
@@ -231,19 +286,99 @@ FROM empleados
 departamento en algún momento, pero han vuelto al mismo puesto, es decir al mismo 
 departamento con el mismo tipo de trabajo.*/
 
+SELECT emp_no, trab_cod, dpto_cod
+FROM empleados
+INTERSECT
+SELECT emp_no, trab_cod, dpto_cod
+FROM historial_laboral;
+/*
+no se ha seleccionado ninguna fila
+*/
+
 /*Ejercicio_5:*/
 /*Obtén los empleados que no han cambiado de puesto ninguna vez. Visualizar el 
 código del empleado, el nombre del empleado y el código del trabajo.*/
+
+SELECT emp_no, nombre 
+FROM empleados
+MINUS
+SELECT h.emp_no, e.nombre
+FROM historial_laboral h, empleados e
+WHERE h.emp_no = e.emp_no;--join
+/*
+no se ha seleccionado ninguna fila
+*/
+--No es posible sacar los que nunca han cambiado pidiendo que se seleccionen el trab_cod
+
+
+/*modificación del ejercicio:*/
+
+INSERT INTO EMPLEADOS (EMP_NO, NOMBRE, APELLIDO1, APELLIDO2, TRAB_COD, DPTO_COD)
+            VALUES('5555', 'LOLA', 'AGUIRRE', 'AGUIRRE', 'T4', 10);
+
+SELECT *
+FROM empleados;
+/*
+EMP_NO NOMBRE     APELLIDO1       APELLIDO2       DIRECC                    CIUDAD               MUNICIPIO            COD_P S FECHA_NA TRA    SALARIO DIR      DPTO_COD
+------ ---------- --------------- --------------- ------------------------- -------------------- -------------------- ----- - -------- --- ---------- ------ ----------
+1111   PEPE       GOMEZ           ARROYO                                                                                    H 20/09/45 T2        3400                10
+2222   ANA        GONZALEZ        LOPEZ           DATO,5                    VITORIA                                   01013 H 20/09/45 T1        1200 1111           20
+3333   LUIS       NEGRO           RUIZ            POSTAS,23                 VITORIA                                   01014 H 20/09/45 T3        2500 1111           10
+4444   ROSA       REY             JIMENEZ                                                                                   H 20/09/45 T3        2200 2222           30
+5555   LOLA       AGUIRRE         AGUIRRE                                                                                              T4                            10
+*/
+
+          
+SELECT emp_no, nombre
+FROM empleados
+WHERE emp_no IN (SELECT emp_no
+                 FROM empleados
+                 MINUS
+                 SELECT h.emp_no
+                 FROM historial_laboral h);
+/*
+EMP_NO NOMBRE    
+------ ----------   
+5555   LOLA  
+*/
 
 /*Ejercicio_6:*/
 /*Muestra empleados que han cambiado de puesto de trabajo en la empresa. Visualiza 
 el código del empleado, el nombre del empleado, el código del trabajo y el código 
 y nombre del departamento. Ordena la salida por código de empleado.*/
 
+SELECT h.emp_no, e.nombre, h.trab_cod, d.dnombre
+FROM empleados e, departs d, historial_laboral h
+WHERE h.emp_no = e.emp_no
+AND h.dpto_cod = d.dept_no
+ORDER BY h.emp_no, h.dpto_cod;
+/*
+EMP_NO NOMBRE     TRA DNOMBRE       
+------ ---------- --- --------------
+1111   PEPE       T1  CONTABILIDAD  
+1111   PEPE       T1  CONTABILIDAD  
+1111   PEPE       T2  INVESTIGACION 
+2222   ANA        T5  CONTABILIDAD  
+2222   ANA        T1  CONTABILIDAD  
+3333   LUIS       T5  INVESTIGACION 
+3333   LUIS       T2  VENTAS        
+4444   ROSA       T3  CONTABILIDAD 
+*/
+
 /*Ejercicio_7:*/
-/*¿Qué hace la siguiente sentencia?:
-        SELECT EMP_NO, TRAB_COD, SALARIO
-        FROM EMPLEADOS
-        UNION
-        SELECT EMP_NO, TRAB_COD, 0
-        FROM HISTORIAL_LABORAL;*/
+/*¿Qué hace la siguiente sentencia?:*/
+
+SELECT EMP_NO, TRAB_COD, SALARIO
+FROM EMPLEADOS
+UNION
+SELECT EMP_NO, TRAB_COD, 0
+FROM HISTORIAL_LABORAL;
+/*Visualizar el codigo del empleado, codigo trabajo y salario. Enunciado 
+ejercicio 5*/        
+
+---------------------------------------------------------------------------------
+
+/*Sacar los datos de los empleados que trabajan en un departamento cuya localidad
+tenga también almacenes. En nuestra base de datos ha aparecido una nueva tabla
+llamada almacenes que continene el código del almacén, numérico de 3, la 
+descripción alfanumerico de 10 y la localidad alfanumérico de 14.*/
